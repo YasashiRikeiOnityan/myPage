9794e41cbeba480f902b5693f97f18591fc7156f
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2023-08-04 23:19:35 +0900

    something

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-<<<<<<< HEAD
   transpileDependencies: [
     'vuetify'
   ]
-=======
-  transpileDependencies: true
->>>>>>> 5210a77970e8b1e9507c350b0bc730fc45dc504a
 })
